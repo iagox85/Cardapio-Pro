e96a030afdcb96ca63c429daf35dee5ead5cb7e7
iagox85 <iagox85@gmail.com>
2026-06-28 20:45:38 -0300

    produtos.js

---
+++ produtos.js
@@ -0,0 +1,66 @@
+const produtos = [
+
+    {
+        id:1,
+        nome:"X-Burguer",
+        categoria:"Hambúrgueres",
+        descricao:"Pão, carne, queijo e molho especial.",
+        preco:29.90,
+        imagem:"https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=600"
+    },
+
+    {
+        id:2,
+        nome:"X-Salada",
+        categoria:"Hambúrgueres",
+        descricao:"Hambúrguer completo com alface e tomate.",
+        preco:33.90,
+        imagem:"https://images.unsplash.com/photo-1550547660-d9450f859349?w=600"
+    },
+
+    {
+        id:3,
+        nome:"Batata Frita",
+        categoria:"Porções",
+        descricao:"Batata frita crocante 400g.",
+        preco:22.00,
+        imagem:"https://images.unsplash.com/photo-1573080496219-bb080dd4f877?w=600"
+    },
+
+    {
+        id:4,
+        nome:"Onion Rings",
+        categoria:"Porções",
+        descricao:"Anéis de cebola empanados.",
+        preco:24.90,
+        imagem:"https://images.unsplash.com/photo-1639024471283-03518883512d?w=600"
+    },
+
+    {
+        id:5,
+        nome:"Coca-Cola Lata",
+        categoria:"Bebidas",
+        descricao:"350ml",
+        preco:7.00,
+        imagem:"https://images.unsplash.com/photo-1629203851122-3726ecdf080e?w=600"
+    },
+
+    {
+        id:6,
+        nome:"Guaraná Lata",
+        categoria:"Bebidas",
+        descricao:"350ml",
+        preco:6.50,
+        imagem:"https://images.unsplash.com/photo-1624517452488-04869289c4ca?w=600"
+    },
+
+    {
+        id:7,
+        nome:"Milk Shake",
+        categoria:"Sobremesas",
+        descricao:"Chocolate 500ml",
+        preco:18.90,
+        imagem:"https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=600"
+    }
+
+];
