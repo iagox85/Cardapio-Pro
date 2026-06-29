69c76df4fb3d355c33aa0a6fbb191cd498596557
iagox85 <iagox85@gmail.com>
2026-06-29 16:50:17 -0300

    admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,21 @@
-document.getElementById("sair").addEventListener("click", async () => {
-  await supabaseClient.auth.signOut();
-  window.location.href = "login.html";
-});
+async function protegerPaginaAdmin() {
+  const {
+    data: { session }
+  } = await supabaseClient.auth.getSession();
+
+  if (!session) {
+    window.location.href = "login.html";
+    return;
+  }
+}
+
+protegerPaginaAdmin();
+
+const botaoSair = document.getElementById("sair");
+
+if (botaoSair) {
+  botaoSair.addEventListener("click", async () => {
+    await supabaseClient.auth.signOut();
+    window.location.href = "login.html";
+  });
+}
