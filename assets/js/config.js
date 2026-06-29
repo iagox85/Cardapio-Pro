9ff7a2f0a8b4c64434500bdd64efccaab75a956e
iagox85 <iagox85@gmail.com>
2026-06-29 14:18:25 -0300

    config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
-  url: "https://qsvztcjqywvcwvstswpx.supabase.co/rest/v1/L",
+  url: "https://qsvztcjqywvcwvstswpx.supabase.co",
   publishableKey: "sb_publishable_FflgRWdkFbue8lbNXPK31Q_v27-Cbk1"
 };
